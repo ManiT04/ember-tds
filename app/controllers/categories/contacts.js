34e3a1623895972c2c10d22c51ae62b833501ecd
Manivone <21902892@etu.unicaen.fr>
2021-02-22 17:12:58 +0100

    nom des onglets=cat

--- app/controllers/categories/contacts.js
+++ app/controllers/categories/contacts.js
@@ -2,12 +2,18 @@ import Controller from '@ember/controller';
 
 export default class CategoriesContactsController extends Controller {
   //Liste initiale des contacts (initialisée depuis le setupController de la route)
-  //@tracked datas;
+  /*@tracked datas; //ex : <Input @value={{@model.nom}}/>
 
   //Liste filtrée des contacts non supprimés
-  get contacts(){}
+  get contacts(){
+    return this.datas.filterBy('isDeleted',false);
+  }
   //Liste des contacts marqués comme supprimés.
-  get deleteteds(){}
+  get deleteteds(){
+    return this.datas.filterBy('isDeleted',true);
+  }
   //Nombre de contacts marqués comme supprimés.
-  get deletetedsCount(){}
+  get deletetedsCount(){
+    return this.deleteds.length;
+  }*/
 }
