51c6b048e5a224c5ae14d3ae3e4d8098d49d6a46
Manivone <21902892@etu.unicaen.fr>
2021-03-18 23:51:09 +0100

    Modification sur les section, add, addProduct

--- app/routes/sections/add.js
+++ app/routes/sections/add.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
 
 export default class SectionsAddRoute extends Route {
+  model(){
+    return {};
+  }
+
+  @action save(data){
+    let section = this.store.createRecord('section',data);
+    section.save().then(() => this.transitionTo('sections'));
+  }
+
 }
