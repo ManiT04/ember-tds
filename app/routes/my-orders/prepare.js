6ec51b474db0e4ad419bf995e6a866deaea3d9b5
Manivone <21902892@etu.unicaen.fr>
2021-04-07 21:51:13 +0200

    modification de la section avancement commande

--- app/routes/my-orders/prepare.js
+++ app/routes/my-orders/prepare.js
@@ -1,9 +1,17 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class MyOrdersPrepareRoute extends Route {
 
   model(params) {
     return this.store.findRecord('order', params.order_id, {include : 'orderdetails.product,user'} );
   }
+
+  @action prepare(order) {
+    order.status = "created";
+    this.transitionTo('myOrders',order.id);
+    console.log("created");
+  }
+
 }
